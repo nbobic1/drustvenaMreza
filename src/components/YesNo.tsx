9d51fd42e66ad8d197ae1a13a6ffd0746ab48d9e
nbobic1 <92312723+nbobic1@users.noreply.github.com>
2023-03-28 00:17:42 +0200

    veliki skok

--- src/components/YesNo.tsx
+++ src/components/YesNo.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Button, View } from 'react-native';
+import { C } from '../utils/Consts';
 import { YesNoQElement } from '../utils/DataTypes';
 import ButtonV1 from './BasicComponents/ButtonV1';
 import Row from './BasicComponents/Row';
@@ -7,19 +7,21 @@ import Row from './BasicComponents/Row';
 
 type Props = {
     value: YesNoQElement;
-    saveE: boolean;
 };
 
 
-const YesNo = ({ value, saveE }: Props) => {
+const YesNo = ({ value }: Props) => {
     const [btn, setBtn] = useState(true);
-    useEffect(() => {
-        value.answer = btn;
-    }, [saveE])
     return (
         <Row >
-            <ButtonV1 onPress={() => { setBtn(!btn) }} title="Yes"></ButtonV1>
-            <ButtonV1 onPress={() => { setBtn(!btn) }} title="No" ></ButtonV1>
+            <ButtonV1 bg={btn ? C.btnT : C.white} v={2} onPress={() => {
+                value.answer = true;
+                setBtn(true);
+            }} title="Yes"></ButtonV1>
+            <ButtonV1 bg={btn ? C.white : C.btnT} v={2} onPress={() => {
+                setBtn(false);
+                value.answer = false;
+            }} title="No" ></ButtonV1>
         </Row>
     );
 };
